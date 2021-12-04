0778660103f60260c14ee91c7ad5ade4245cfe31
Ed <37041178+drum445@users.noreply.github.com>
2021-12-04 18:58:07 +0000

    Update part_2.rb

--- 02/part_2.rb
+++ 02/part_2.rb
@@ -3,19 +3,19 @@ data = STDIN.map(&:chomp)
 pos = {"x" => 0, "y" => 0, "aim" => 0}
 
 data.each do |line|
-	cmd = line.split(" ")
-	action = cmd[0]
-	x = cmd[1].to_i
+    cmd = line.split(" ")
+    action = cmd[0]
+    x = cmd[1].to_i
 
-	case cmd[0]
-	when "forward"
-		pos["x"] += x
-		pos["y"] += pos["aim"] * x
-	when "up"
-		pos["aim"] -= x
-	when "down"
-		pos["aim"] += x
-	end
+    case cmd[0]
+    when "forward"
+        pos["x"] += x
+        pos["y"] += pos["aim"] * x
+    when "up"
+        pos["aim"] -= x
+    when "down"
+        pos["aim"] += x
+    end
 end
 
 p pos
