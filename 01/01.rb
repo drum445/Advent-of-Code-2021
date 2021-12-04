47ae4c47a7fcf91a16d1b56d753f2b7ef47e2074
Ed <37041178+drum445@users.noreply.github.com>
2021-12-04 18:37:40 +0000

    Update 01.rb

--- 01/01.rb
+++ 01/01.rb
@@ -1,20 +1,3 @@
-data = File.readlines("input.txt", chomp: true)
+data = STDIN.map {|line| line.chomp.to_i}
 
-data = data.map(&:to_i)
-
-# part 1
 p data.each_with_index.map { |d, i| 1 if d > data[i-1]; }.compact.size
-
-# part 2
-increases = 0
-prev = nil
-data.each_with_index do |_, i|
-	window = [data[i], data[i+1], data[i+2]]
-
-	break if window.include?(nil)
-
-	increases += 1 if prev && window.sum > prev
-	prev = window.sum
-end
-
-p increases
