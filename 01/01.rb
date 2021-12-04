bfaabe5ca7df15201c583b8690ca3f3d4b7a6cc8
Ed <37041178+drum445@users.noreply.github.com>
2021-12-04 18:15:08 +0000

    Update 01.rb

--- 01/01.rb
+++ 01/01.rb
@@ -1,3 +1,20 @@
 data = File.readlines("input.txt", chomp: true)
 
-p data.each_with_index.map { |d, i| 1 if d.to_i > data[i-1].to_i; }.compact.size
+data = data.map(&:to_i)
+
+# part 1
+p data.each_with_index.map { |d, i| 1 if d > data[i-1]; }.compact.size
+
+# part 2
+increases = 0
+prev = nil
+data.each_with_index do |_, i|
+	window = [data[i], data[i+1], data[i+2]]
+
+	break if window.include?(nil)
+
+	increases += 1 if prev && window.sum > prev
+	prev = window.sum
+end
+
+p increases
